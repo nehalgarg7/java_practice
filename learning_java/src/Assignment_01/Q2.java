eb8ce95d37288e0c36202da53c4f7067859e3ea3
nehalgarg7 <100798031+nehalgarg7@users.noreply.github.com>
2022-05-08 13:33:43 +0530

    Update Q2.java

--- learning_java/src/Assignment_01/Q2.java
+++ learning_java/src/Assignment_01/Q2.java
@@ -1,1 +1,25 @@
 
+//WAP to count the total number of calls for a member function from more than one objects.
+
+class Function_Call{
+  static int count=0;
+  Function_Call(){
+    
+  };
+
+  void display(){
+    count++;
+  }
+}
+
+class Q2{
+  public static void main(String args[]){
+    Function_Call obj1 = new Function_Call();
+    obj1.display();
+    Function_Call obj2 = new Function_Call();
+    obj2.display();
+    Function_Call obj3 = new Function_Call();
+    obj3.display();
+    System.out.println("Total number of call are " + Function_Call.count );
+  }
+}
