6e3021c8cff6a29ba9dd69f18294e0aea951cf38
nehalgarg7 <100798031+nehalgarg7@users.noreply.github.com>
2022-05-29 23:37:46 +0530

    Create P16_MultiThreading.java

---
+++ learning_java/src/Unit_03/P16_MultiThreading.java
@@ -0,0 +1,56 @@
+package unit_03;
+
+/*
+ * MultiProcessing:
+ * -Sequencial MultiProcessing
+ * -Parallel MultiProcessing
+ * -Concurrent Multiprocessing
+ * 
+ * MultiTasking:
+ * -Sequencial MultiTasking
+ * -Parallel MultiTasking (Not Applicable)
+ * -Concurrent MultiTasking
+ * 
+ */
+
+public class P16_MultiThreading {
+		public static void main(String[] args)
+		{
+			
+			MultiThreadingInJava obj = new MultiThreadingInJava();
+			obj.threadConcept();
+			obj.threadSynchronization();
+			obj.interThreadCommunication();
+			obj.ProducerConsumerProblems();
+			obj.Wait_And_Notify();
+		}
+}
+
+
+class MultiThreadingInJava
+{
+	void threadConcept()
+	{
+		
+	}
+	
+	void threadSynchronization()
+	{
+		
+	}
+	
+	void interThreadCommunication()
+	{
+		
+	}
+	
+	void ProducerConsumerProblems()
+	{
+		 
+	}
+	
+	void Wait_And_Notify()
+	{
+		
+	}
+}
